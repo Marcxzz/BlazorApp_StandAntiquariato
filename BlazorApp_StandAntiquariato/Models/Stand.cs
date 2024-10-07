95c7b439fa709bc9354080dddf60016e1ffef362
Marcxzz <angionimarco2.0@gmail.com>
2024-10-07 09:40:33 +0200

    Aggiungere i file di progetto.

---
+++ BlazorApp_StandAntiquariato/Models/Stand.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_StandAntiquariato.Models
+{
+	public class Stand
+	{
+        public int IdStand { get; set; }
+        public char ZonaEsposizione { get; set; }
+        public int Superficie { get; set; }
+        public string? NomeAntiquario { get; set; }
+        public string? CognomeAntiquario { get; set; }
+        public bool Coperto { get; set; }
+        public DateTime DataInizio { get; set; }
+    }
+}
